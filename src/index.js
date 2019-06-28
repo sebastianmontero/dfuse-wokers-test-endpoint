87a224a688913fe2390f3f5887958f2a31dc1483
Sebastian Montero <smontero@mightywizards.com>
2019-06-28 18:40:08 -0500

    Developed server to work as dfuse worker test endpoint

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Processed data ok!');
+});
+
+app.listen(port, () => console.log(`Dfuse Test endpoint listening on port: ${port}`));
