f6e7263e275d5e422fedfced18adae7e6ae06bef
Sebastian Montero <smontero@mightywizards.com>
2019-06-29 17:35:49 -0500

    Added console log to print matchingActions

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const port = 3000;
 app.use(express.json());
 
 app.post('/', (req, res) => {
-    console.dir(req.body);
+    console.log(req.body);
+    const { trace: { matchingActions } } = req.body;
+    console.log(matchingActions);
     res.send('Processed data ok!');
 });
 
