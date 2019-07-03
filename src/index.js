8adbe546b1d9e3e32609a56e52ae6c3975ff1801
Sebastian Montero <smontero@mightywizards.com>
2019-07-03 16:26:47 -0500

    Fixed issue when printing new json

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.post('/', (req, res) => {
     console.log('Body: ', req.body);
     const { trace: { matchingActions } } = req.body;
     console.log('Matching Actions: ', matchingActions);
-    const { dbOps, dbOps: { newJSON } } = matchingActions[0];
+    const { dbOps } = matchingActions[0];
     const { newJSON } = dbOps[0];
     console.log('Db Ops: ', dbOps);
     console.log('New JSON: ', newJSON);
